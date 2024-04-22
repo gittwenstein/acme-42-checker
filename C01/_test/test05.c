ccb8130488f25fb320d84af8e623fcea5a35ca7e
gittwenstein <maxian@post.cz>
2024-04-22 14:58:05 +0200

    Refactor test05, add norminette switch, add check_diff function to test script

---
+++ C01/_test/test05.c
@@ -0,0 +1,8 @@
+#include <assert.h>
+
+void ft_putstr(char *str);
+
+int main(int count, char **args)
+{
+	ft_putstr(args[1]);
+}
--- _test/test05.c
+++
@@ -1,9 +0,0 @@
-#include <assert.h>
-
-void ft_putstr(char *str);
-
-int main(void)
-{
-	char *test = (char *)"\"This sentence has five words.\"\t";
-    ft_putstr(test);
-}
