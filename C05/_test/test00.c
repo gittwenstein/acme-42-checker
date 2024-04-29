40b958cf4014bbca2aaba1ebe8575803672ba42a
gittwenstein <maxian@post.cz>
2024-04-29 12:20:28 +0200

    Fix test for factorial

--- C05/_test/test00.c
+++ C05/_test/test00.c
@@ -9,7 +9,7 @@ int main(int count, char **args)
 {
 	assert(ft_iterative_factorial(-1) == 0);
 	assert(ft_iterative_factorial(-1123) == 0);
-	assert(ft_iterative_factorial(0) == 0);
+	assert(ft_iterative_factorial(0) == 1);
 	assert(ft_iterative_factorial(1) == 1);
 	assert(ft_iterative_factorial(2) == 2);
 	assert(ft_iterative_factorial(3) == 6);
--- C05/_test/test01.c
+++ C05/_test/test01.c
@@ -9,7 +9,7 @@ int main(int count, char **args)
 {
 	assert(ft_recursive_factorial(-1) == 0);
 	assert(ft_recursive_factorial(-1123) == 0);
-	assert(ft_recursive_factorial(0) == 0);
+	assert(ft_recursive_factorial(0) == 1);
 	assert(ft_recursive_factorial(1) == 1);
 	assert(ft_recursive_factorial(2) == 2);
 	assert(ft_recursive_factorial(3) == 6);
